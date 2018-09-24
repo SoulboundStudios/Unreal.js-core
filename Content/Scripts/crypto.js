83a662fba60632cc6a4f6755cc4e3dbab95df8d4
vrachels <victor@soulboundstudios.com>
2018-09-24 12:17:47 -0700

    md5 accepts string type

--- Content/Scripts/crypto.js
+++ Content/Scripts/crypto.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Hash {
     update(data) {
+		if (typeof data === 'string')
+            data = Uint8Array.from(Array.from(data, c => c.charCodeAt(0)));
+		
+		
         if (!this.data)
             this.data = new Unreal.JavascriptMD5();
         memory.exec(data.buffer, () => {
@@ -10,7 +14,7 @@ class Hash {
         return this;
     }
     digest(encoding = "hex") {
-        const ret = this.data ? this.data.Final() : '0xd41d8cd98f00b204e9800998ecf8427e';
+        const ret = this.data ? this.data.Final() : 'd41d8cd98f00b204e9800998ecf8427e';
         return ret;
     }
 }
